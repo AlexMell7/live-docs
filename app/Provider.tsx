62799d717c6b029f20b3ccd6a8ebfa733fbc3a37
Alex <melnykoleksandr547@gmail.com>
2025-07-08 18:51:15 +0200

    Implement Authentication,Document editor,Collaborative Room,Edit document title

---
+++ app/Provider.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React, { ReactNode } from "react";
+import { ClientSideSuspense, LiveblocksProvider } from "@liveblocks/react/suspense";
+import Loader from "@/components/Loader";
+import { getCLerkUser } from "@/lib/actions/user.actions";
+const Provider = ({children}:{children:ReactNode}) => {
+  return (
+    <LiveblocksProvider
+     authEndpoint="/api/liveblocks-auth"
+     resolveUsers={async ({userIds})=>{
+      const users = await getCLerkUser({userIds});
+      return users
+     }}
+    >
+      <ClientSideSuspense fallback={<Loader/>}>
+        {children}
+      </ClientSideSuspense>
+    </LiveblocksProvider>
+  );
+};
+
+export default Provider;
